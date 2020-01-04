c6cb201d8b917bdb73a3717390b68c893f55759b
titanservers <54300844+titanservers@users.noreply.github.com>
2020-01-04 11:43:50 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -183,7 +183,6 @@ cd $HOME
 msg -ne "Key: "
 wget -O $HOME/lista-arq ${REQUEST}/lista-arq > /dev/null 2>&1 && echo -e "\033[1;32m Verified" || {
    echo -e "\033[1;32m Verified"
-   invalid_key
    exit
    }
 sleep 1s
