3da81558b6b9eeebb12558157910b6d28e5c0c65
titanservers <54300844+titanservers@users.noreply.github.com>
2020-01-08 23:40:59 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -48,6 +48,44 @@ AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCO
 }
 
 
+mm_decho (){
+local i stepping
+stepping="0.01"
+
+# When first argument is empty or not given, it just echoes a new line
+# and leaves.
+if [ ! "$1" ]; then 
+echo
+return
+fi
+
+# If a second argument is given (delay stepping), check it for validity
+# (if it is a float) and set stepping according to the argument.
+if (( $# > 1 )) && 
+[[ ($2 = $(echo $2 | grep -oE '[[:digit:]]')) ||
+($2 = $(echo $2 | grep -oE '[[:digit:]]+\.[[:digit:]]+')) ]] 
+then
+stepping="$2"
+# In case the previous test failed, but we have a second argument,
+# meaning it is invalid, just print the message, complain a bit and then
+# quit the function with false.
+elif (( $# > 1 )); then
+echo "$1"
+echo ".! mm_decho() oops: second argument is invalid!" 1>&2
+echo ".! must be /float but is: \"$2\", leaving function.." 1>&2
+return false 2>/dev/null
+fi
+
+# Do delayed printing of first input argument. Calculate the
+# length of first arg. and substract one. Then make it a /for/
+# sequence going through all the characters of the string,
+# printing these and wait the delay stepping time.
+for i in $(seq 0 $((${#1}-1))); do
+echo -ne "${1:$i:1}"
+sleep $stepping
+done
+echo
+}
 
 ####INSTALAR PAQUETES PARA BANER Y AVISOS 
 
